7f4a2327f3bd356663bee6aad4874fe5451d5d79
canvassed <tydickson255@gmail.com>
2023-12-06 20:40:37 -0500

    setup of documents

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>canvassed</title>
-    <link rel="stylesheet" href="./style.css">
-    <link rel="icon" href="./smalllogo.jpg" type="image/x-icon">
-  </head>
-  <body>
-    <main>
-        <h1>Welcome to canvassed</h1>  
-    </main>
-	<script src="index.js"></script>
-  </body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>canvassed</title>
+        <link rel="stylesheet" href="./style.css">
+        <link rel="icon" href="./smalllogo.jpg" type="image/x-icon">
+    </head>
+    <body>
+        <div id="header_div">
+            <h1>canvassed</h1>
+        </div>
+        <div id="what_is_div">
+
+        </div>
+        <div id="socials_div">
+
+        </div>
+        <div id="contact_div">
+
+        </div>
+        <div id="create_account_div">
+
+        </div>
+    </body>
 </html>
